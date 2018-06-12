d44604b28400b3ed7222f96b75c078a7e3eedace
Andre Abrantes <andredossantosabrantes@gmail.com>
2018-06-12 14:38:42 -0300

    Add link component to account options

--- react/components/AccountOptions.js
+++ react/components/AccountOptions.js
@@ -1,38 +1,49 @@
-
-import React, { Component } from 'react'
-import { injectIntl, intlShape } from 'react-intl'
-import { Button } from 'vtex.styleguide'
+import React, { Component } from "react";
+import { injectIntl, intlShape } from "react-intl";
+import { Button } from "vtex.styleguide";
+import { Link } from 'render';
 
 // Component that shows account options to the user.
 class AccountOptions extends Component {
-    static propTypes = {
-        /** Intl object*/
-        intl: intlShape
-    }
+  static propTypes = {
+    /** Intl object*/
+    intl: intlShape
+  };
 
-    handleClickButton = path => location.assign(path)
+  handleClickButton = path => location.assign(path);
 
-    render() {
-        const { intl: { formatMessage } } = this.props
-        return (
-            <div className="vtex-login__account-options items-center w-100">
-                <div className="ma4 min-h-2 b--light-gray">
-                    <Button variation="tertiary" size="small"
-                        onClick={() => this.handleClickButton('account')}
-                    >
-                        <div className="f7">{formatMessage({ id: 'login.my-account' })}</div>
-                    </Button>
-                </div>
-                <div className="ma4 min-h-2 b--light-gray">
-                    <Button variation="tertiary" size="small"
-                        onClick={() => this.handleClickButton('account/orders')}
-                    >
-                        <div className="f7">{formatMessage({ id: 'login.my-orders' })}</div>
-                    </Button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const {
+      intl: { formatMessage }
+    } = this.props;
+    return (
+      <div className="vtex-login__account-options items-center w-100">
+        <div className="ma4 min-h-2 b--light-gray">
+          <Link page={"store/account"}>
+            <Button
+              variation="tertiary"
+              size="small"
+              onClick={() => this.handleClickButton("account")}
+            >
+              <div className="f7">
+                {formatMessage({ id: "login.my-account" })}
+              </div>
+            </Button>
+          </Link>
+        </div>
+        <div className="ma4 min-h-2 b--light-gray">
+          <Link page={"store/account/orders"}>
+            <Button
+              variation="tertiary"
+              size="small"
+            >
+              <div className="f7">{formatMessage({ id: "login.my-orders" })}</div>
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default injectIntl(AccountOptions)
+export default injectIntl(AccountOptions);
