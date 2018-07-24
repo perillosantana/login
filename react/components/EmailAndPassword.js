e001a59d188df2cad60b10d8f60a2f03429a05aa
Bruno Dias <bruno.dias@vtex.com.br>
2018-07-24 14:58:54 -0300

    Fix onClick handle of recovery password

--- react/components/EmailAndPassword.js
+++ react/components/EmailAndPassword.js
@@ -63,7 +63,11 @@ class EmailAndPassword extends Component {
   }
 
   handleCreatePassword = event => {
-    this.props.onStateChange({ step: steps.EMAIL_VERIFICATION, isCreatePassword: true })
+    this.props.onStateChange({
+      step: steps.EMAIL_VERIFICATION,
+      isCreatePassword: true,
+      isOnInitialScreen: false,
+    })
     event.preventDefault()
   }
 
