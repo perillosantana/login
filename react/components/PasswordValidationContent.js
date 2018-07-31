fc02c36d819afb7d88045dfe6aba1807401a6206
Vitória Heliane <vitoriaheliane@gmail.com>
2018-07-31 17:35:51 -0300

    Update the render items

--- react/components/PasswordValidationContent.js
+++ react/components/PasswordValidationContent.js
@@ -3,26 +3,22 @@ import PropTypes from 'prop-types'
 import PasswordValidationItem from './PasswordValidationItem'
 
 export default class PasswordValidationContent extends Component {
-  renderItems = fields => fields.map(
-    field => (
-      <div className="mt2" key={field.id}>
-        <PasswordValidationItem
-          label={field.label}
-          prefix={field.prefix}
-          valid={field.valid}
-        />
-      </div>
-    )
-  )
-
   render() {
     const { fields } = this.props
 
-    const content = this.renderItems(fields)
-
     return (
       <Fragment>
-        {content}
+        {fields.map(
+          field => (
+            <div className="mt2" key={field.id}>
+              <PasswordValidationItem
+                label={field.label}
+                prefix={field.prefix}
+                valid={field.valid}
+              />
+            </div>
+          )
+        )}
       </Fragment>
     )
   }
