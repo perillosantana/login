a9aaae199d59f7faca208d7383a3861e7e115361
Vitória Heliane <vitoriaheliane@gmail.com>
2018-09-24 10:29:30 -0300

    Use ramda in the getProfile method

--- react/utils/profile.js
+++ react/utils/profile.js
@@ -1,9 +1,10 @@
-export const getProfile = data => {
-  let profile = null
+import { path } from 'ramda'
 
-  if (data && !data.loading && data.getSession && data.getSession.profile &&
-    (data.getSession.profile.id || data.getSession.profile.email)) {
-    profile = data.getSession.profile
+export const getProfile = data => {
+  let profile = path(['getSession', 'profile'], data)
+
+  if (!path(['id'], profile) && !path(['email'], profile)) {
+    profile = null
   }
 
   return profile
