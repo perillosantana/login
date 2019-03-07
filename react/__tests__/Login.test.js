ef541314a3e1c6c2873a9c94b9420cd688c2e288
Gustavo Alves <gustavo.alves@splab.ufcg.edu.br>
2019-03-07 15:33:09 -0300

    Add tests with data

--- react/__tests__/Login.test.js
+++ react/__tests__/Login.test.js
@@ -15,4 +15,54 @@ describe('<Login /> component', () => {
     )
     expect(asFragment()).toMatchSnapshot()
   })
+
+  it('should match snapshot without data', () => {
+    const { asFragment } = renderWithIntl(
+      <Login
+        data={{
+          loading: false,
+          refetch: () => {},
+        }}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should match snapshot with data only email', () => {
+    const { asFragment } = renderWithIntl(
+      <Login
+        data={{
+          loading: false,
+          refetch: () => {},
+          getSession: {
+            profile: {
+              email: 'email@vtex.com',
+              id: 'id',
+            },
+          },
+        }}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should match snapshot with data', () => {
+    const { asFragment } = renderWithIntl(
+      <Login
+        data={{
+          loading: false,
+          refetch: () => {},
+          getSession: {
+            profile: {
+              email: 'email@vtex.com',
+              firstName: 'firstName',
+              lastName: 'lastName',
+              id: 'id',
+            },
+          },
+        }}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
 })
