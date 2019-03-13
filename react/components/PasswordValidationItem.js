98921f39dc20ea63134996d65befa73b6bb0cb8b
Ana Luiza <analuizamga10@hotmail.com>
2019-03-13 18:19:20 -0300

    Fix input to show error only when user has already typed

--- react/components/PasswordValidationItem.js
+++ react/components/PasswordValidationItem.js
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types'
 export default class PasswordValidationItem extends Component {
   render() {
     const { valid, prefix, label } = this.props
-
     return (
-      <div className={`flex flex-row ${valid ? 'c-success' : 'c-danger'}`}>
+      <div
+        className={`flex flex-row ${
+          valid !== undefined ? (valid ? 'c-success' : 'c-danger') : 'c-muted-2'
+        }`}
+      >
         <div className="w-20">
           <span className="t-small">{prefix}</span>
         </div>
         <div className="w-80">
           <span className="t-small">{label}</span>
         </div>
-      </div >
+      </div>
     )
   }
 }
