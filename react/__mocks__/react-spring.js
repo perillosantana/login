90e007f7c32a1eab55c376c310bc385a451adf3d
Juan Almeida <juan.satyro@gmail.com>
2019-03-14 15:50:10 -0300

    Fixing mocks

--- react/__mocks__/react-spring.js
+++ react/__mocks__/react-spring.js
@@ -1,2 +1,9 @@
 import React from 'react'
-export const Transition = props => <div {...props}>{props.children}</div>
+export const Transition = props => {
+  const mockedStyle = { opacity: 0, transform: 'translateX(-50%)' }
+  return (
+    <div {...props}>
+      {props.children.map(children => children(mockedStyle))}
+    </div>
+  )
+}
