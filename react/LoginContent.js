6675b6f72c833b2de1649e29ca6d85ec1e41aacb
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2019-05-22 09:22:55 -0300

    reloading page after login

--- react/LoginContent.js
+++ react/LoginContent.js
@@ -232,6 +232,7 @@ class LoginContent extends Component {
     const { loginCallback } = this.props
     return this.context.patchSession().then(() => {
       if (loginCallback) {
+        window.location.reload()
         loginCallback()
       } else {
         // the use of location.assign here, instead of
